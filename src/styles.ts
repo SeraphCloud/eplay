c8cd73c5bad7f58ba0529b231351103f98b91be3
Devv-ictor <dev.victor.alexandre@gmail.com>
2025-11-25 12:09:24 -0300

    first commit: cria o header e variáveis CSS

---
+++ src/styles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
+
+export const cores = {
+  branca: '#eeeeee',
+  preto: '#111111',
+  cinza: '#333333',
+  verde: '#10ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    background-color: ${cores.preto};
+    color: ${cores.branca};
+    padding-top: 40px;
+  }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
