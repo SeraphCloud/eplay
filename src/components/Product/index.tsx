617f26f21082413f6cff21c20c3a637c85c8452f
Devv-ictor <dev.victor.alexandre@gmail.com>
2025-11-27 16:42:33 -0300

    Style: Cria a componentização dos cards e tags

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,18 +1,34 @@
 import Tag from '../Tag'
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Infos, Titulo } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="https://placehold.it/222x250" alt="game-cover" />
-    <Titulo>Nome do jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, incidunt
-      ab, molestias perferendis iure ipsum molestiae tenetur, nihil pariatur
-      dolorum asperiores. Corporis numquam aliquid est itaque, obcaecati dolorum
-      ut voluptate!
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
